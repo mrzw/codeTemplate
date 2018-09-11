fb4d3de02acab4b1f9b3bb4c84d4f5c2bc34ef2a
mrzw <2463846947@qq.com>
2018-09-11 17:48:27 +0800

    2018-09-11

--- LRU实现/LRU.cpp
+++ LRU实现/LRU.cpp
@@ -2,37 +2,44 @@
 #include <unordered_map>
 #include <list>
 using namespace std;
-// 基于双向链表和hash_map构建LRU算法
-class LRUCache {
-private:
-    int m_capacity;
-    unordered_map<int, list<pair<int, int>>::iterator> m_map; // m_map_iter->first: key, m_map_iter->second: list iterator;
-    list<pair<int, int>> m_list; // m_list_iter->first: key, m_list_iter->second: value;
+
+class LRU {
 public:
-    LRUCache(int capacity) : m_capacity(capacity) {}
-    // Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-    int get(int key) {
-        auto found_iter = m_map.find(key);
-        if (found_iter == m_map.end()) return -1;
-        m_list.splice(m_list.begin(), m_list, found_iter->second);
-        found_iter->second = m_list.begin();
-        return found_iter->second->second;
+    int capacity_;
+    typedef list<pair<int,string>>::iterator LIST_ITER;
+    unordered_map<int,LIST_ITER> mp_;
+    list<pair<int,string>> list_;
+
+    LRU(int capacity):capacity_(capacity) {}
+    string get(int key) {
+        auto found_iter = mp_.find(key);
+        if(found_iter==mp_.end()) return "";
+        string res = found_iter->second->second;
+        list_.erase(mp_[key]);  //先删除原来的数据
+        list_.push_front(make_pair(key,res)); //将数据插入到头部
+        mp_[key] = list_.begin(); // 更新hashmap
+        return res;
     }
-    // Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
-    void set(int key, int value) {
-        auto found_iter = m_map.find(key);
-        if(found_iter != m_map.end()) {
-            m_list.splice(m_list.begin(), m_list, found_iter->second);
-            found_iter->second = m_list.begin();
-            found_iter->second->second = value;
-            return;
+    void set(int key, string value) {
+        auto found_iter = mp_.find(key);
+        if(found_iter!=mp_.end()) {
+            string res = found_iter->second->second;
+            list_.erase(mp_[key]);  //先删除原来的数据
+            list_.push_front(make_pair(key,res)); //将数据插入到头部
+            mp_[key] = list_.begin(); // 更新hashmap
+        } else {
+            if(mp_.size()>=capacity_) {
+                int key_to_del = list_.back().first;
+                list_.pop_back(); // 删除链表尾部数据
+                mp_.erase(key_to_del); // 删除链表尾部元素对应的hashmap
+            }
+            list_.push_front(make_pair(key,value)); // 更新插入的元素到链表
+            mp_.insert(make_pair(key,list_.begin())); // 更新插入的元素到hashmap
         }
-        if(m_map.size() == m_capacity) {
-            int key_to_del = m_list.back().first;
-            m_list.pop_back();
-            m_map.erase(key_to_del);
-        }
-        m_list.emplace_front(key, value);
-        m_map[key] = m_list.begin();
     }
 };
+
+int main() {
+
+    return 0;
+}
